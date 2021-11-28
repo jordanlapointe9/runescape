b68d5a8cadd7c50e20a20cabce83425a472f5b8f
jordanlapointe9 <jordan.lapointe87@gmail.com>
2021-11-28 00:53:14 -0500

    Version 1

---
+++ js/scroll.js
@@ -0,0 +1,11 @@
+/* Fichier JavaScript nécessaire pour le fonctionnement du déroulement fluide lors du clic sur les flèches */
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
